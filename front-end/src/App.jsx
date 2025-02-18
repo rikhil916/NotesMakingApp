e3eae135c77b8dcb097a5ec534b80355309ee02a
Chetan Rikhil <chetanrikhil916@gmail.com>
2025-02-18 20:32:20 +0530

    first

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/layout/Home';
